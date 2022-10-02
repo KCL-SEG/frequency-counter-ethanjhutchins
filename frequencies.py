2b95bba2f98e6a1434d6697e005c2a5c8795744d
Ethan Hutchins <ethanjameshutchins@gmail.com>
2022-10-02 22:30:53 +0100

    Fix frequency functionality.

--- frequencies.py
+++ frequencies.py
@@ -5,10 +5,14 @@ def frequencies(items):
     frequencies = {}
     # Your code goes here
     for i in range (len(items)):
+        flag = False
+        freqKeys = frequencies.keys()
+        for j in freqKeys:
+            if str(items[i]) == j:
+                flag = True
         x = frequencies.setdefault(str(items[i]), 1)
-        if x:
-            freq = frequencies.get(str(items[i]))
+        if (flag):
             frequencies.pop(str(items[i]))
-            frequencies[str(items[i])] = freq + 1
+            frequencies[str(items[i])] = x + 1
 
     return frequencies
