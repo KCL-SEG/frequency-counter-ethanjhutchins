23d27412b42e79b02948247f8d85940653af0b65
Ethan Hutchins <ethanjameshutchins@gmail.com>
2022-10-02 20:18:46 +0100

    Add main functionality for function

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,11 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for i in range (len(items)):
+        x = frequencies.setdefault(str(items[i]), 1)
+        if x:
+            freq = frequencies.get(str(items[i]))
+            frequencies.pop(str(items[i]))
+            frequencies[str(items[i])] = freq + 1
+
     return frequencies
